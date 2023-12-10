ec7bf175679925c33de93e484a898257420fe670
Thomas Brouwer <brouwer.thomas@gmail.com>
2023-12-10 14:49:02 +0100

    Building dataset complete

--- spotify/main.py
+++ spotify/main.py
@@ -1,127 +1,47 @@
 """Main file for the spotify package."""
 
-
-from pathlib import Path
-from typing import Any
-
-import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
-from spotify.utils import load_credentials
-
-
-def export_to_json(data: dict[Any, Any] | list[dict[Any, Any]], name: str) -> None:
-    """Export dictionary to JSON file."""
-    import json
-
-    with Path(f"./data/{name}.json").open("w") as f:
-        json.dump(data, f)
-
-
-# Load credentials from config.ini file
-credentials = load_credentials("spotify")
-
-# Pass credentials to Spotipy API
-spotipy_client_id = credentials["SPOTIPY_CLIENT_ID"]
-spotipy_client_secret = credentials["SPOTIPY_CLIENT_SECRET"]
-spotipy_redirect_uri = credentials["SPOTIPY_REDIRECT_URI"]
-
-# Authentication
-client_credentials_manager = SpotifyClientCredentials(
-    client_id=spotipy_client_id, client_secret=spotipy_client_secret
+from utils import (
+    calculate_first_playlist_track_overlap_percentage,
+    calculate_total_overlap_percentage,
+    load_credentials,
 )
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# Get playlist URI
-playlist_link = "https://open.spotify.com/playlist/2flYqzsxSNSIHjCNCphCMw?si=6408cf90576944be"
-playlist_uri = playlist_link.split("/")[-1].split("?")[0]
-track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_uri)["items"]]
 
-# for track in sp.playlist_tracks(playlist_URI)["items"]:
-#     # URI
-#     track_uri = track["track"]["uri"]
+def main() -> None:
+    """Extract song statistics from Spotify playlist."""
+    # Load credentials and authenticate with Spotify
+    credentials = load_credentials("spotify")
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=credentials["SPOTIPY_CLIENT_ID"],
+        client_secret=credentials["SPOTIPY_CLIENT_SECRET"],
+    )
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-#     # Track name
-#     track_name = track["track"]["name"]
-#     print(track_name)
+    # Playlist: Pallen 2023
+    playlist_uri = "https://open.spotify.com/playlist/2flYqzsxSNSIHjCNCphCMw?si=6408cf90576944be"
 
-#     # Main Artist
-#     artist_uri = track["track"]["artists"][0]["uri"]
-#     artist_info = sp.artist(artist_uri)
+    # Fetch playlist details
+    # df_playlist = fetch_playlist_details(sp, playlist_uri)
+    # df_playlist.to_csv("./data/playlist_stats.csv", index=False)
 
-#     # Name, popularity, genre
-#     artist_name = track["track"]["artists"][0]["name"]
-#     artist_pop = artist_info["popularity"]
-#     artist_genres = artist_info["genres"]
+    # Compare playlists
+    total_overlap = calculate_total_overlap_percentage(
+        sp=sp,
+        first_playlist_uri=playlist_uri,
+        second_playlist_uri="https://open.spotify.com/playlist/4gjdA4c6B0YB7S5GmgxLEk?si=070cab20524f4ddc",
+    )
+    print(f"Overlap: {total_overlap:.2f}%")
 
-#     # Album
-#     album = track["track"]["album"]["name"]
+    # Compare playlists
+    playlist_hype = calculate_first_playlist_track_overlap_percentage(
+        sp=sp,
+        first_playlist_uri="https://open.spotify.com/playlist/4gjdA4c6B0YB7S5GmgxLEk?si=070cab20524f4ddc",
+        second_playlist_uri=playlist_uri,
+    )
+    print(f"Playlist hype (Pallen 2023 vs. 3voor12 SvhJ 2023): {playlist_hype:.2f}%")
 
-#     # Popularity of the track
-#     track_pop = track["track"]["popularity"]
 
-# print(track_name)
-
-# Initialize empty DataFrame for aggregated features (genres, artists)
-df_aggregated = pd.DataFrame()
-
-# Iterate over tracks in playlist
-for track in sp.playlist_tracks(playlist_uri)["items"]:
-    # Export to JSON
-    # export_to_json(track, "track")
-
-    # Get track URI
-    track_uri = track.get("track").get("uri")
-
-    # Get artists
-    artists_dict = track.get("track").get("artists")
-    artists_list = [artist.get("name") for artist in artists_dict]
-    artists_uri_list = [artist.get("uri") for artist in artists_dict]
-    artists = ", ".join(artists_list)
-    print(artists_list)
-
-    # Get artist info
-    artists_info = [sp.artist(artist_uri) for artist_uri in artists_uri_list]
-    genres = [artist.get("genres") for artist in artists_info]
-    artists_popularity = [artist.get("popularity") for artist in artists_info]
-    # export_to_json(artists_info, "artists")
-
-    # Get categories (???)
-    categories = sp.categories()
-    print(categories)
-    # export_to_json(categories, "categories")
-
-    # Get track duration in minutes
-    duration_ms = track.get("track").get("duration_ms")
-    duration_min = duration_ms / 60000
-    duration_sec = (duration_min - int(duration_min)) * 60
-    duration = f"{int(duration_min)}:{int(duration_sec)}"
-
-    # Get audio features
-    audio_features = sp.audio_features(track_uri)[0]
-    # export_to_json(audio_features, "audio_features")
-
-    # Build dictionary with track stats
-    stats_dict = {
-        "track_name": track.get("track").get("name"),
-        "track_artists": artists_list,
-        "track_album": track.get("track").get("album").get("name"),
-        "release_date": track.get("track").get("album").get("release_date"),
-        "album_type": track.get("track").get("album").get("album_type"),
-        "duration_min": duration,
-        "added_by_user_id": track.get("added_by").get("id"),
-        "added_at": track.get("added_at"),
-        "track_popularity": track.get("track").get("popularity"),
-        "track_id": track.get("track").get("id"),
-        "danceability": audio_features.get("danceability"),
-        "energy": audio_features.get("energy"),
-        "acousticness": audio_features.get("acousticness"),
-        "tempo": audio_features.get("tempo"),
-        "genres": genres,
-        "artists_popularity": artists_popularity,
-    }
-
-    # print(stats_dict)
-
-    break
+if __name__ == "__main__":
+    main()
--- spotify/utils.py
+++ spotify/utils.py
@@ -1,14 +1,21 @@
 """Utility functions for the Spotipy API."""
 
+import json
 from configparser import ConfigParser
 from pathlib import Path
+from typing import Any
+
+import numpy as np
+import pandas as pd
+from spotipy.client import Spotify
 
 
 def load_credentials(database: str) -> dict[str, str]:
     """Load credentials from config.ini file."""
     config_path = Path("./config.ini")
     if not config_path.exists():
-        print(f"Config file {config_path} does not exist!")
+        msg = f"Config file {config_path} does not exist!"
+        raise FileNotFoundError(msg)
 
     credentials = ConfigParser()
     credentials.read(config_path)
@@ -18,3 +25,131 @@ def load_credentials(database: str) -> dict[str, str]:
         "SPOTIPY_CLIENT_SECRET": credentials[database]["SPOTIPY_CLIENT_SECRET"],
         "SPOTIPY_REDIRECT_URI": credentials[database]["SPOTIPY_REDIRECT_URI"],
     }
+
+
+def get_playlist_track_uris(sp: Spotify, playlist_uri: str) -> list[str]:
+    """Retrieve all track IDs from a Spotify playlist, handling pagination."""
+    track_uris = []
+    response = sp.playlist_tracks(playlist_uri)
+    while response:
+        track_uris += [item["track"]["uri"] for item in response["items"] if item["track"]]
+        if response["next"]:
+            response = sp.next(response)
+        else:
+            break
+    return track_uris
+
+
+def export_to_json(data: dict[Any, Any] | list[dict[Any, Any]], name: str) -> None:
+    """Export dictionary or list of dictionaries to a JSON file."""
+    with Path(f"./data/{name}.json").open("w") as f:
+        json.dump(data, f)
+
+
+def transform_track_duration(track_duration_ms: int) -> str:
+    """Transform track duration from ms to minutes."""
+    duration_min = track_duration_ms / 60000
+    duration_sec = (duration_min - int(duration_min)) * 60
+    return f"{int(duration_min)}:{int(duration_sec)}"
+
+
+def fetch_playlist_details(sp: Spotify, playlist_uri: str) -> pd.DataFrame:
+    """Fetch details of each track in a playlist and return as a pandas DataFrame."""
+    # Initialize pandas dataframe
+    df_playlist = pd.DataFrame()
+    # Use the Spotify API to fetch the playlist details, handling pagination
+    response = sp.playlist_tracks(playlist_uri)
+    # Iterate tracks in playlist
+    while response:
+        for track in response["items"]:
+            if track:
+                track_details = fetch_track_details(sp, track)
+                df_playlist = pd.concat([df_playlist, pd.Series(track_details)], axis=1)
+        # Check if there's more tracks to load (pagination)
+        if response["next"]:
+            response = sp.next(response)
+        else:
+            break
+    return df_playlist.T
+
+
+def fetch_track_details(sp: Spotify, track: dict[Any, Any]) -> dict[str, Any]:
+    """Fetch track details."""
+    # Fetch track URI
+    track_uri = track["track"]["uri"]
+    # Fetch artist URIs
+    artists_uri_list = [artist["uri"] for artist in track["track"]["artists"]]
+    # Build list of nested dictionaries with artist details
+    artists = []
+    for artist_uri in artists_uri_list:
+        artist = {
+            "name": sp.artist(artist_uri)["name"],
+            "genres": sp.artist(artist_uri)["genres"],
+            "popularity": sp.artist(artist_uri)["popularity"],
+        }
+        # Append artist to list of dictionaries
+        artists.append(artist)
+    # Create artists label
+    artists_list = [artist["name"] for artist in artists]
+    artists_label = ", ".join(artists_list)
+    # Calculate average artist popularity
+    artists_popularity = np.mean([artist["popularity"] for artist in artists])
+    # Fetch audio features
+    audio_features = sp.audio_features(track_uri)[0]
+    # Fetch user details
+    user_details = sp.user(track["added_by"]["id"])
+    # Print track details
+    print(f"Fetching details for: {track['track']['name']} - {artists_label}")
+    # Build the stats dictionary
+    return {
+        "name": track["track"]["name"],
+        "artist": artists_label,
+        "album": track["track"]["album"]["name"],
+        "album_type": track["track"]["album"]["album_type"],
+        "release_date": track["track"]["album"]["release_date"],
+        "duration": transform_track_duration(track["track"]["duration_ms"]),
+        "added_at": track["added_at"],
+        "added_by": user_details["display_name"],
+        "track_popularity": track["track"]["popularity"],
+        "avg_artist_popularity": artists_popularity,
+        "danceability": audio_features["danceability"],
+        "energy": audio_features["energy"],
+        "key": audio_features["key"],
+        "loudness": audio_features["loudness"],
+        "mode": audio_features["mode"],
+        "speechiness": audio_features["speechiness"],
+        "acousticness": audio_features["acousticness"],
+        "instrumentalness": audio_features["instrumentalness"],
+        "liveness": audio_features["liveness"],
+        "valence": audio_features["valence"],
+        "tempo": audio_features["tempo"],
+        "time_signature": audio_features["time_signature"],
+        "track_id": track["track"]["id"],
+        "artists": artists,
+    }
+
+
+def calculate_total_overlap_percentage(
+    sp: Spotify, first_playlist_uri: str, second_playlist_uri: str
+) -> float:
+    """Calculate the total overlap percentage between two playlists, considering the unique tracks in both."""
+    first_playlist_track_ids = get_playlist_track_uris(sp, first_playlist_uri)
+    second_playlist_track_ids = get_playlist_track_uris(sp, second_playlist_uri)
+    common_tracks = set(first_playlist_track_ids).intersection(second_playlist_track_ids)
+    # Calculate the total number of unique tracks in both playlists
+    total_unique_tracks = len(set(first_playlist_track_ids + second_playlist_track_ids))
+    # Calculate the overlap percentage: common tracks / total unique tracks in both playlists
+    return len(common_tracks) / total_unique_tracks * 100
+
+
+def calculate_first_playlist_track_overlap_percentage(
+    sp: Spotify, first_playlist_uri: str, second_playlist_uri: str
+) -> float:
+    """Calculate the percentage of tracks from the first playlist that are also present in the second playlist."""
+    first_playlist_track_ids = get_playlist_track_uris(sp, first_playlist_uri)
+    second_playlist_track_ids = get_playlist_track_uris(sp, second_playlist_uri)
+    common_tracks = set(first_playlist_track_ids).intersection(second_playlist_track_ids)
+    # Calculate the total number of unique tracks in the first playlist also present in the second
+    first_playlist_unique_tracks = len(first_playlist_track_ids)
+    # Calculate the overlap percentage: common tracks / total unique tracks in first playlist
+    return len(common_tracks) / first_playlist_unique_tracks * 100
